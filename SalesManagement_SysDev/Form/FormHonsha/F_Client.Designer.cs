4dbdfa1b10b6e25ab0c4a6da03bd2c1bb6b4193c
1D04KSk-sana <C2105@oic.jp>
2023-10-04 17:33:50 +0900

    フォーム仮作成

---
+++ SalesManagement_SysDev/Form/FormHonsha/F_Client.Designer.cs
@@ -0,0 +1,356 @@
+﻿namespace SalesManagement_SysDev
+{
+    partial class F_HonshaClient
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnReturn = new System.Windows.Forms.Button();
+            this.pnlSelect = new System.Windows.Forms.Panel();
+            this.rdbRegister = new System.Windows.Forms.RadioButton();
+            this.rdbUpdate = new System.Windows.Forms.RadioButton();
+            this.rdbSearch = new System.Windows.Forms.RadioButton();
+            this.rdbHidden = new System.Windows.Forms.RadioButton();
+            this.lblClientID = new System.Windows.Forms.Label();
+            this.txbClientID = new System.Windows.Forms.TextBox();
+            this.lblClientName = new System.Windows.Forms.Label();
+            this.txbClientName = new System.Windows.Forms.TextBox();
+            this.lblSalesOfficeID = new System.Windows.Forms.Label();
+            this.txbCilentPhone = new System.Windows.Forms.TextBox();
+            this.cmbSalesOfficeID = new System.Windows.Forms.ComboBox();
+            this.lblCilentPhone = new System.Windows.Forms.Label();
+            this.lblClientPostal = new System.Windows.Forms.Label();
+            this.txbClientPostal = new System.Windows.Forms.TextBox();
+            this.lblClientAddress = new System.Windows.Forms.Label();
+            this.txbClientAddress = new System.Windows.Forms.TextBox();
+            this.lblClientFax = new System.Windows.Forms.Label();
+            this.txbClientFax = new System.Windows.Forms.TextBox();
+            this.lblClientHidden = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.btnDone = new System.Windows.Forms.Button();
+            this.pnlSelect.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnReturn
+            // 
+            this.btnReturn.Font = new System.Drawing.Font("MS UI Gothic", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.btnReturn.Location = new System.Drawing.Point(12, 12);
+            this.btnReturn.Name = "btnReturn";
+            this.btnReturn.Size = new System.Drawing.Size(94, 38);
+            this.btnReturn.TabIndex = 0;
+            this.btnReturn.Text = "戻る";
+            this.btnReturn.UseVisualStyleBackColor = true;
+            // 
+            // pnlSelect
+            // 
+            this.pnlSelect.BackColor = System.Drawing.SystemColors.ButtonShadow;
+            this.pnlSelect.Controls.Add(this.rdbHidden);
+            this.pnlSelect.Controls.Add(this.rdbSearch);
+            this.pnlSelect.Controls.Add(this.rdbUpdate);
+            this.pnlSelect.Controls.Add(this.rdbRegister);
+            this.pnlSelect.Location = new System.Drawing.Point(158, 12);
+            this.pnlSelect.Name = "pnlSelect";
+            this.pnlSelect.Size = new System.Drawing.Size(349, 37);
+            this.pnlSelect.TabIndex = 1;
+            // 
+            // rdbRegister
+            // 
+            this.rdbRegister.AutoSize = true;
+            this.rdbRegister.Font = new System.Drawing.Font("MS UI Gothic", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.rdbRegister.Location = new System.Drawing.Point(15, 8);
+            this.rdbRegister.Name = "rdbRegister";
+            this.rdbRegister.Size = new System.Drawing.Size(65, 22);
+            this.rdbRegister.TabIndex = 0;
+            this.rdbRegister.TabStop = true;
+            this.rdbRegister.Text = "登録";
+            this.rdbRegister.UseVisualStyleBackColor = true;
+            // 
+            // rdbUpdate
+            // 
+            this.rdbUpdate.AutoSize = true;
+            this.rdbUpdate.Font = new System.Drawing.Font("MS UI Gothic", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.rdbUpdate.Location = new System.Drawing.Point(97, 8);
+            this.rdbUpdate.Name = "rdbUpdate";
+            this.rdbUpdate.Size = new System.Drawing.Size(65, 22);
+            this.rdbUpdate.TabIndex = 1;
+            this.rdbUpdate.TabStop = true;
+            this.rdbUpdate.Text = "更新";
+            this.rdbUpdate.UseVisualStyleBackColor = true;
+            // 
+            // rdbSearch
+            // 
+            this.rdbSearch.AutoSize = true;
+            this.rdbSearch.Font = new System.Drawing.Font("MS UI Gothic", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.rdbSearch.Location = new System.Drawing.Point(185, 8);
+            this.rdbSearch.Name = "rdbSearch";
+            this.rdbSearch.Size = new System.Drawing.Size(65, 22);
+            this.rdbSearch.TabIndex = 2;
+            this.rdbSearch.TabStop = true;
+            this.rdbSearch.Text = "検索";
+            this.rdbSearch.UseVisualStyleBackColor = true;
+            // 
+            // rdbHidden
+            // 
+            this.rdbHidden.AutoSize = true;
+            this.rdbHidden.Font = new System.Drawing.Font("MS UI Gothic", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.rdbHidden.Location = new System.Drawing.Point(266, 8);
+            this.rdbHidden.Name = "rdbHidden";
+            this.rdbHidden.Size = new System.Drawing.Size(83, 22);
+            this.rdbHidden.TabIndex = 3;
+            this.rdbHidden.TabStop = true;
+            this.rdbHidden.Text = "非表示";
+            this.rdbHidden.UseVisualStyleBackColor = true;
+            // 
+            // lblClientID
+            // 
+            this.lblClientID.AutoSize = true;
+            this.lblClientID.Font = new System.Drawing.Font("MS UI Gothic", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.lblClientID.Location = new System.Drawing.Point(13, 79);
+            this.lblClientID.Name = "lblClientID";
+            this.lblClientID.Size = new System.Drawing.Size(60, 18);
+            this.lblClientID.TabIndex = 2;
+            this.lblClientID.Text = "顧客ID";
+            // 
+            // txbClientID
+            // 
+            this.txbClientID.Font = new System.Drawing.Font("MS UI Gothic", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.txbClientID.Location = new System.Drawing.Point(79, 76);
+            this.txbClientID.Name = "txbClientID";
+            this.txbClientID.Size = new System.Drawing.Size(100, 25);
+            this.txbClientID.TabIndex = 3;
+            // 
+            // lblClientName
+            // 
+            this.lblClientName.AutoSize = true;
+            this.lblClientName.Font = new System.Drawing.Font("MS UI Gothic", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.lblClientName.Location = new System.Drawing.Point(229, 79);
+            this.lblClientName.Name = "lblClientName";
+            this.lblClientName.Size = new System.Drawing.Size(62, 18);
+            this.lblClientName.TabIndex = 4;
+            this.lblClientName.Text = "顧客名";
+            // 
+            // txbClientName
+            // 
+            this.txbClientName.Font = new System.Drawing.Font("MS UI Gothic", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.txbClientName.Location = new System.Drawing.Point(297, 76);
+            this.txbClientName.Name = "txbClientName";
+            this.txbClientName.Size = new System.Drawing.Size(147, 25);
+            this.txbClientName.TabIndex = 5;
+            // 
+            // lblSalesOfficeID
+            // 
+            this.lblSalesOfficeID.AutoSize = true;
+            this.lblSalesOfficeID.Font = new System.Drawing.Font("MS UI Gothic", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.lblSalesOfficeID.Location = new System.Drawing.Point(485, 79);
+            this.lblSalesOfficeID.Name = "lblSalesOfficeID";
+            this.lblSalesOfficeID.Size = new System.Drawing.Size(78, 18);
+            this.lblSalesOfficeID.TabIndex = 6;
+            this.lblSalesOfficeID.Text = "営業所ID";
+            // 
+            // txbCilentPhone
+            // 
+            this.txbCilentPhone.Font = new System.Drawing.Font("MS UI Gothic", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.txbCilentPhone.Location = new System.Drawing.Point(867, 76);
+            this.txbCilentPhone.Name = "txbCilentPhone";
+            this.txbCilentPhone.Size = new System.Drawing.Size(147, 25);
+            this.txbCilentPhone.TabIndex = 7;
+            // 
+            // cmbSalesOfficeID
+            // 
+            this.cmbSalesOfficeID.Font = new System.Drawing.Font("MS UI Gothic", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.cmbSalesOfficeID.FormattingEnabled = true;
+            this.cmbSalesOfficeID.Location = new System.Drawing.Point(569, 76);
+            this.cmbSalesOfficeID.Name = "cmbSalesOfficeID";
+            this.cmbSalesOfficeID.Size = new System.Drawing.Size(156, 26);
+            this.cmbSalesOfficeID.TabIndex = 8;
+            // 
+            // lblCilentPhone
+            // 
+            this.lblCilentPhone.AutoSize = true;
+            this.lblCilentPhone.Font = new System.Drawing.Font("MS UI Gothic", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.lblCilentPhone.Location = new System.Drawing.Point(781, 79);
+            this.lblCilentPhone.Name = "lblCilentPhone";
+            this.lblCilentPhone.Size = new System.Drawing.Size(80, 18);
+            this.lblCilentPhone.TabIndex = 9;
+            this.lblCilentPhone.Text = "電話番号";
+            // 
+            // lblClientPostal
+            // 
+            this.lblClientPostal.AutoSize = true;
+            this.lblClientPostal.Font = new System.Drawing.Font("MS UI Gothic", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.lblClientPostal.Location = new System.Drawing.Point(13, 121);
+            this.lblClientPostal.Name = "lblClientPostal";
+            this.lblClientPostal.Size = new System.Drawing.Size(80, 18);
+            this.lblClientPostal.TabIndex = 10;
+            this.lblClientPostal.Text = "郵便番号";
+            // 
+            // txbClientPostal
+            // 
+            this.txbClientPostal.Font = new System.Drawing.Font("MS UI Gothic", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.txbClientPostal.Location = new System.Drawing.Point(99, 118);
+            this.txbClientPostal.Name = "txbClientPostal";
+            this.txbClientPostal.Size = new System.Drawing.Size(100, 25);
+            this.txbClientPostal.TabIndex = 11;
+            // 
+            // lblClientAddress
+            // 
+            this.lblClientAddress.AutoSize = true;
+            this.lblClientAddress.Font = new System.Drawing.Font("MS UI Gothic", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.lblClientAddress.Location = new System.Drawing.Point(229, 121);
+            this.lblClientAddress.Name = "lblClientAddress";
+            this.lblClientAddress.Size = new System.Drawing.Size(44, 18);
+            this.lblClientAddress.TabIndex = 12;
+            this.lblClientAddress.Text = "住所";
+            // 
+            // txbClientAddress
+            // 
+            this.txbClientAddress.Font = new System.Drawing.Font("MS UI Gothic", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.txbClientAddress.Location = new System.Drawing.Point(297, 118);
+            this.txbClientAddress.Name = "txbClientAddress";
+            this.txbClientAddress.Size = new System.Drawing.Size(428, 25);
+            this.txbClientAddress.TabIndex = 13;
+            // 
+            // lblClientFax
+            // 
+            this.lblClientFax.AutoSize = true;
+            this.lblClientFax.Font = new System.Drawing.Font("MS UI Gothic", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.lblClientFax.Location = new System.Drawing.Point(781, 121);
+            this.lblClientFax.Name = "lblClientFax";
+            this.lblClientFax.Size = new System.Drawing.Size(40, 18);
+            this.lblClientFax.TabIndex = 14;
+            this.lblClientFax.Text = "FAX";
+            // 
+            // txbClientFax
+            // 
+            this.txbClientFax.Font = new System.Drawing.Font("MS UI Gothic", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.txbClientFax.Location = new System.Drawing.Point(867, 118);
+            this.txbClientFax.Name = "txbClientFax";
+            this.txbClientFax.Size = new System.Drawing.Size(147, 25);
+            this.txbClientFax.TabIndex = 15;
+            // 
+            // lblClientHidden
+            // 
+            this.lblClientHidden.AutoSize = true;
+            this.lblClientHidden.Font = new System.Drawing.Font("MS UI Gothic", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.lblClientHidden.Location = new System.Drawing.Point(13, 161);
+            this.lblClientHidden.Name = "lblClientHidden";
+            this.lblClientHidden.Size = new System.Drawing.Size(98, 18);
+            this.lblClientHidden.TabIndex = 16;
+            this.lblClientHidden.Text = "非表示理由";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("MS UI Gothic", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.textBox1.Location = new System.Drawing.Point(117, 158);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(612, 25);
+            this.textBox1.TabIndex = 17;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 196);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(1042, 286);
+            this.dataGridView1.TabIndex = 18;
+            // 
+            // btnDone
+            // 
+            this.btnDone.Font = new System.Drawing.Font("MS UI Gothic", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.btnDone.Location = new System.Drawing.Point(960, 12);
+            this.btnDone.Name = "btnDone";
+            this.btnDone.Size = new System.Drawing.Size(94, 38);
+            this.btnDone.TabIndex = 19;
+            this.btnDone.Text = "実行";
+            this.btnDone.UseVisualStyleBackColor = true;
+            // 
+            // F_HonshaClient
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1066, 494);
+            this.Controls.Add(this.btnDone);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.lblClientHidden);
+            this.Controls.Add(this.txbClientFax);
+            this.Controls.Add(this.lblClientFax);
+            this.Controls.Add(this.txbClientAddress);
+            this.Controls.Add(this.lblClientAddress);
+            this.Controls.Add(this.txbClientPostal);
+            this.Controls.Add(this.lblClientPostal);
+            this.Controls.Add(this.lblCilentPhone);
+            this.Controls.Add(this.cmbSalesOfficeID);
+            this.Controls.Add(this.txbCilentPhone);
+            this.Controls.Add(this.lblSalesOfficeID);
+            this.Controls.Add(this.txbClientName);
+            this.Controls.Add(this.lblClientName);
+            this.Controls.Add(this.txbClientID);
+            this.Controls.Add(this.lblClientID);
+            this.Controls.Add(this.pnlSelect);
+            this.Controls.Add(this.btnReturn);
+            this.Name = "F_HonshaClient";
+            this.Text = "F_Client";
+            this.pnlSelect.ResumeLayout(false);
+            this.pnlSelect.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnReturn;
+        private System.Windows.Forms.Panel pnlSelect;
+        private System.Windows.Forms.RadioButton rdbHidden;
+        private System.Windows.Forms.RadioButton rdbSearch;
+        private System.Windows.Forms.RadioButton rdbUpdate;
+        private System.Windows.Forms.RadioButton rdbRegister;
+        private System.Windows.Forms.Label lblClientID;
+        private System.Windows.Forms.TextBox txbClientID;
+        private System.Windows.Forms.Label lblClientName;
+        private System.Windows.Forms.TextBox txbClientName;
+        private System.Windows.Forms.Label lblSalesOfficeID;
+        private System.Windows.Forms.TextBox txbCilentPhone;
+        private System.Windows.Forms.ComboBox cmbSalesOfficeID;
+        private System.Windows.Forms.Label lblCilentPhone;
+        private System.Windows.Forms.Label lblClientPostal;
+        private System.Windows.Forms.TextBox txbClientPostal;
+        private System.Windows.Forms.Label lblClientAddress;
+        private System.Windows.Forms.TextBox txbClientAddress;
+        private System.Windows.Forms.Label lblClientFax;
+        private System.Windows.Forms.TextBox txbClientFax;
+        private System.Windows.Forms.Label lblClientHidden;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button btnDone;
+    }
+}
---
+++ SalesManagement_SysDev/Form/FormHonsha/F_Honsha.Designer.cs
@@ -0,0 +1,59 @@
+﻿namespace SalesManagement_SysDev
+{
+    partial class F_Honsha
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnFormClient = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnFormClient
+            // 
+            this.btnFormClient.Location = new System.Drawing.Point(287, 12);
+            this.btnFormClient.Name = "btnFormClient";
+            this.btnFormClient.Size = new System.Drawing.Size(236, 67);
+            this.btnFormClient.TabIndex = 0;
+            this.btnFormClient.Text = "顧客管理画面";
+            this.btnFormClient.UseVisualStyleBackColor = true;
+            // 
+            // F_Honsha
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnFormClient);
+            this.Name = "F_Honsha";
+            this.Text = "F_Honsha";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnFormClient;
+    }
+}
