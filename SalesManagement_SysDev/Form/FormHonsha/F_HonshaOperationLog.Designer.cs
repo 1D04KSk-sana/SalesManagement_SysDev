6a44cc397d2b221db9a3d7a916b566bb95437813
hasegawa <c2258@oic.jp>
2023-11-02 15:28:02 +0900

    フォーム作成

--- SalesManagement_SysDev/Form/FormHonsha/F_HonshaOperationLog.Designer.cs
+++ SalesManagement_SysDev/Form/FormHonsha/F_HonshaOperationLog.Designer.cs
@@ -28,12 +28,333 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.pnlHonsha = new System.Windows.Forms.Panel();
+            this.btnReturn = new System.Windows.Forms.Button();
+            this.btnClose = new System.Windows.Forms.Button();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.btnDone = new System.Windows.Forms.Button();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.cmbView = new System.Windows.Forms.ComboBox();
+            this.txbEmployeeID = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.dgvOperatinLog = new System.Windows.Forms.DataGridView();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.dateTimePicker2 = new System.Windows.Forms.DateTimePicker();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lblPageSize = new System.Windows.Forms.Label();
+            this.txbPageSize = new System.Windows.Forms.TextBox();
+            this.btnPageSize = new System.Windows.Forms.Button();
+            this.txbNumPage = new System.Windows.Forms.TextBox();
+            this.lblNumPage = new System.Windows.Forms.Label();
+            this.btnPageMin = new System.Windows.Forms.Button();
+            this.btnBack = new System.Windows.Forms.Button();
+            this.btnNext = new System.Windows.Forms.Button();
+            this.btnPageMax = new System.Windows.Forms.Button();
+            this.pnlHonsha.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvOperatinLog)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pnlHonsha
+            // 
+            this.pnlHonsha.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(124)))), ((int)(((byte)(255)))), ((int)(((byte)(112)))));
+            this.pnlHonsha.Controls.Add(this.btnReturn);
+            this.pnlHonsha.Controls.Add(this.btnClose);
+            this.pnlHonsha.Location = new System.Drawing.Point(1, 1);
+            this.pnlHonsha.Name = "pnlHonsha";
+            this.pnlHonsha.Size = new System.Drawing.Size(1230, 90);
+            this.pnlHonsha.TabIndex = 0;
+            // 
+            // btnReturn
+            // 
+            this.btnReturn.Font = new System.Drawing.Font("MS UI Gothic", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.btnReturn.Location = new System.Drawing.Point(21, 22);
+            this.btnReturn.Name = "btnReturn";
+            this.btnReturn.Size = new System.Drawing.Size(110, 50);
+            this.btnReturn.TabIndex = 1;
+            this.btnReturn.TabStop = false;
+            this.btnReturn.Text = "戻る";
+            this.btnReturn.UseVisualStyleBackColor = true;
+            // 
+            // btnClose
+            // 
+            this.btnClose.Font = new System.Drawing.Font("MS UI Gothic", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.btnClose.Location = new System.Drawing.Point(1095, 22);
+            this.btnClose.Name = "btnClose";
+            this.btnClose.Size = new System.Drawing.Size(110, 50);
+            this.btnClose.TabIndex = 2;
+            this.btnClose.TabStop = false;
+            this.btnClose.Text = "閉じる";
+            this.btnClose.UseVisualStyleBackColor = true;
+            // 
+            // btnClear
+            // 
+            this.btnClear.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(96)))), ((int)(((byte)(172)))), ((int)(((byte)(242)))));
+            this.btnClear.Font = new System.Drawing.Font("MS UI Gothic", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.btnClear.Location = new System.Drawing.Point(909, 113);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(110, 50);
+            this.btnClear.TabIndex = 4;
+            this.btnClear.TabStop = false;
+            this.btnClear.Text = "クリア";
+            this.btnClear.UseVisualStyleBackColor = false;
+            // 
+            // btnDone
+            // 
+            this.btnDone.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(143)))), ((int)(((byte)(143)))));
+            this.btnDone.Font = new System.Drawing.Font("MS UI Gothic", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.btnDone.Location = new System.Drawing.Point(1034, 113);
+            this.btnDone.Name = "btnDone";
+            this.btnDone.Size = new System.Drawing.Size(110, 50);
+            this.btnDone.TabIndex = 5;
+            this.btnDone.TabStop = false;
+            this.btnDone.Text = "実行";
+            this.btnDone.UseVisualStyleBackColor = false;
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Font = new System.Drawing.Font("MS UI Gothic", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.radioButton1.Location = new System.Drawing.Point(96, 122);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(93, 32);
+            this.radioButton1.TabIndex = 6;
+            this.radioButton1.Text = "検索";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // cmbView
+            // 
+            this.cmbView.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cmbView.Font = new System.Drawing.Font("MS UI Gothic", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.cmbView.FormattingEnabled = true;
+            this.cmbView.Location = new System.Drawing.Point(676, 122);
+            this.cmbView.Name = "cmbView";
+            this.cmbView.Size = new System.Drawing.Size(200, 36);
+            this.cmbView.TabIndex = 7;
+            this.cmbView.TabStop = false;
+            // 
+            // txbEmployeeID
+            // 
+            this.txbEmployeeID.Font = new System.Drawing.Font("MS UI Gothic", 11F);
+            this.txbEmployeeID.Location = new System.Drawing.Point(195, 266);
+            this.txbEmployeeID.Name = "txbEmployeeID";
+            this.txbEmployeeID.Size = new System.Drawing.Size(200, 29);
+            this.txbEmployeeID.TabIndex = 8;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("MS UI Gothic", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.label1.Location = new System.Drawing.Point(116, 273);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(73, 22);
+            this.label1.TabIndex = 11;
+            this.label1.Text = "社員ID";
+            // 
+            // dgvOperatinLog
+            // 
+            this.dgvOperatinLog.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvOperatinLog.Location = new System.Drawing.Point(12, 337);
+            this.dgvOperatinLog.Name = "dgvOperatinLog";
+            this.dgvOperatinLog.RowHeadersWidth = 62;
+            this.dgvOperatinLog.RowTemplate.Height = 27;
+            this.dgvOperatinLog.Size = new System.Drawing.Size(1200, 350);
+            this.dgvOperatinLog.TabIndex = 12;
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Font = new System.Drawing.Font("MS UI Gothic", 11F);
+            this.dateTimePicker1.Location = new System.Drawing.Point(478, 266);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(200, 29);
+            this.dateTimePicker1.TabIndex = 13;
+            this.dateTimePicker1.TabStop = false;
+            // 
+            // dateTimePicker2
+            // 
+            this.dateTimePicker2.Font = new System.Drawing.Font("MS UI Gothic", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.dateTimePicker2.Location = new System.Drawing.Point(716, 266);
+            this.dateTimePicker2.Name = "dateTimePicker2";
+            this.dateTimePicker2.Size = new System.Drawing.Size(200, 29);
+            this.dateTimePicker2.TabIndex = 14;
+            this.dateTimePicker2.TabStop = false;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(684, 273);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(26, 18);
+            this.label2.TabIndex = 15;
+            this.label2.Text = "～";
+            // 
+            // lblPageSize
+            // 
+            this.lblPageSize.AutoSize = true;
+            this.lblPageSize.Font = new System.Drawing.Font("MS UI Gothic", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.lblPageSize.Location = new System.Drawing.Point(13, 722);
+            this.lblPageSize.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblPageSize.Name = "lblPageSize";
+            this.lblPageSize.Size = new System.Drawing.Size(119, 22);
+            this.lblPageSize.TabIndex = 57;
+            this.lblPageSize.Text = "1ページ行数";
+            // 
+            // txbPageSize
+            // 
+            this.txbPageSize.Font = new System.Drawing.Font("MS UI Gothic", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.txbPageSize.Location = new System.Drawing.Point(140, 718);
+            this.txbPageSize.Margin = new System.Windows.Forms.Padding(4, 2, 4, 2);
+            this.txbPageSize.Name = "txbPageSize";
+            this.txbPageSize.Size = new System.Drawing.Size(74, 29);
+            this.txbPageSize.TabIndex = 58;
+            this.txbPageSize.TabStop = false;
+            // 
+            // btnPageSize
+            // 
+            this.btnPageSize.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(217)))), ((int)(((byte)(217)))), ((int)(((byte)(217)))));
+            this.btnPageSize.Font = new System.Drawing.Font("MS UI Gothic", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.btnPageSize.Location = new System.Drawing.Point(233, 712);
+            this.btnPageSize.Margin = new System.Windows.Forms.Padding(4, 2, 4, 2);
+            this.btnPageSize.Name = "btnPageSize";
+            this.btnPageSize.Size = new System.Drawing.Size(150, 42);
+            this.btnPageSize.TabIndex = 59;
+            this.btnPageSize.TabStop = false;
+            this.btnPageSize.Text = "行数変更";
+            this.btnPageSize.UseVisualStyleBackColor = false;
+            // 
+            // txbNumPage
+            // 
+            this.txbNumPage.Font = new System.Drawing.Font("MS UI Gothic", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.txbNumPage.Location = new System.Drawing.Point(818, 719);
+            this.txbNumPage.Margin = new System.Windows.Forms.Padding(4, 2, 4, 2);
+            this.txbNumPage.Name = "txbNumPage";
+            this.txbNumPage.Size = new System.Drawing.Size(74, 29);
+            this.txbNumPage.TabIndex = 63;
+            this.txbNumPage.TabStop = false;
+            // 
+            // lblNumPage
+            // 
+            this.lblNumPage.AutoSize = true;
+            this.lblNumPage.Font = new System.Drawing.Font("MS UI Gothic", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.lblNumPage.Location = new System.Drawing.Point(900, 722);
+            this.lblNumPage.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblNumPage.Name = "lblNumPage";
+            this.lblNumPage.Size = new System.Drawing.Size(64, 22);
+            this.lblNumPage.TabIndex = 64;
+            this.lblNumPage.Text = "ページ";
+            // 
+            // btnPageMin
+            // 
+            this.btnPageMin.BackColor = System.Drawing.Color.White;
+            this.btnPageMin.Font = new System.Drawing.Font("MS UI Gothic", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.btnPageMin.Location = new System.Drawing.Point(972, 709);
+            this.btnPageMin.Margin = new System.Windows.Forms.Padding(4, 2, 4, 2);
+            this.btnPageMin.Name = "btnPageMin";
+            this.btnPageMin.Size = new System.Drawing.Size(54, 43);
+            this.btnPageMin.TabIndex = 65;
+            this.btnPageMin.TabStop = false;
+            this.btnPageMin.Text = "|◀";
+            this.btnPageMin.UseVisualStyleBackColor = false;
+            // 
+            // btnBack
+            // 
+            this.btnBack.BackColor = System.Drawing.Color.White;
+            this.btnBack.Font = new System.Drawing.Font("MS UI Gothic", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.btnBack.Location = new System.Drawing.Point(1034, 709);
+            this.btnBack.Margin = new System.Windows.Forms.Padding(4, 2, 4, 2);
+            this.btnBack.Name = "btnBack";
+            this.btnBack.Size = new System.Drawing.Size(54, 43);
+            this.btnBack.TabIndex = 66;
+            this.btnBack.TabStop = false;
+            this.btnBack.Text = "◀";
+            this.btnBack.UseVisualStyleBackColor = false;
+            // 
+            // btnNext
+            // 
+            this.btnNext.BackColor = System.Drawing.Color.White;
+            this.btnNext.Font = new System.Drawing.Font("MS UI Gothic", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.btnNext.Location = new System.Drawing.Point(1096, 709);
+            this.btnNext.Margin = new System.Windows.Forms.Padding(4, 2, 4, 2);
+            this.btnNext.Name = "btnNext";
+            this.btnNext.Size = new System.Drawing.Size(54, 43);
+            this.btnNext.TabIndex = 67;
+            this.btnNext.TabStop = false;
+            this.btnNext.Text = "▶";
+            this.btnNext.UseVisualStyleBackColor = false;
+            // 
+            // btnPageMax
+            // 
+            this.btnPageMax.BackColor = System.Drawing.Color.White;
+            this.btnPageMax.Font = new System.Drawing.Font("MS UI Gothic", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.btnPageMax.Location = new System.Drawing.Point(1158, 709);
+            this.btnPageMax.Margin = new System.Windows.Forms.Padding(4, 2, 4, 2);
+            this.btnPageMax.Name = "btnPageMax";
+            this.btnPageMax.Size = new System.Drawing.Size(54, 43);
+            this.btnPageMax.TabIndex = 68;
+            this.btnPageMax.TabStop = false;
+            this.btnPageMax.Text = "▶|";
+            this.btnPageMax.UseVisualStyleBackColor = false;
+            // 
+            // F_HonshaOperationLog
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 18F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "F_HonshaOperationLog";
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(213)))), ((int)(((byte)(255)))), ((int)(((byte)(218)))));
+            this.ClientSize = new System.Drawing.Size(1230, 769);
+            this.ControlBox = false;
+            this.Controls.Add(this.btnPageMax);
+            this.Controls.Add(this.btnNext);
+            this.Controls.Add(this.btnBack);
+            this.Controls.Add(this.btnPageMin);
+            this.Controls.Add(this.lblNumPage);
+            this.Controls.Add(this.txbNumPage);
+            this.Controls.Add(this.btnPageSize);
+            this.Controls.Add(this.txbPageSize);
+            this.Controls.Add(this.lblPageSize);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.dateTimePicker2);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Controls.Add(this.dgvOperatinLog);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txbEmployeeID);
+            this.Controls.Add(this.cmbView);
+            this.Controls.Add(this.radioButton1);
+            this.Controls.Add(this.btnDone);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.pnlHonsha);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.MaximizeBox = false;
+            this.Name = "F_HonshaOperationLog";
+            this.Text = "ログ管理画面";
+            this.pnlHonsha.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvOperatinLog)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Panel pnlHonsha;
+        private System.Windows.Forms.Button btnReturn;
+        private System.Windows.Forms.Button btnClose;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.Button btnDone;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.ComboBox cmbView;
+        private System.Windows.Forms.TextBox txbEmployeeID;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DataGridView dgvOperatinLog;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.DateTimePicker dateTimePicker2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label lblPageSize;
+        private System.Windows.Forms.TextBox txbPageSize;
+        private System.Windows.Forms.Button btnPageSize;
+        private System.Windows.Forms.TextBox txbNumPage;
+        private System.Windows.Forms.Label lblNumPage;
+        private System.Windows.Forms.Button btnPageMin;
+        private System.Windows.Forms.Button btnBack;
+        private System.Windows.Forms.Button btnNext;
+        private System.Windows.Forms.Button btnPageMax;
     }
 }
