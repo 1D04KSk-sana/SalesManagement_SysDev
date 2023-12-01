abcaf8761b0b29d345bbe971d4339576740b7e1b
C2285@oic.osaka.jp <C2285@oic.osaka.jp>
2023-12-01 17:18:46 +0900

    フォームの作成2

--- SalesManagement_SysDev/Form/FormButuryu/F_ButuryuWarehousing.Designer.cs
+++ SalesManagement_SysDev/Form/FormButuryu/F_ButuryuWarehousing.Designer.cs
@@ -224,12 +224,12 @@
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("MS UI Gothic", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
-            this.label2.Location = new System.Drawing.Point(668, 326);
+            this.label2.Location = new System.Drawing.Point(707, 330);
             this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(178, 24);
+            this.label2.Size = new System.Drawing.Size(130, 24);
             this.label2.TabIndex = 95;
-            this.label2.Text = "受注確定年月日";
+            this.label2.Text = "入庫年月日";
             // 
             // label4
             // 
