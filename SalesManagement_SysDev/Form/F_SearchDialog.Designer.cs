ee7f78591e52a6f7492b2075953f44c84457772c
1D04KSk-sana <C2105@oic.jp>
2023-12-19 23:40:58 +0900

    seachDialog

--- SalesManagement_SysDev/Form/F_SearchDialog.Designer.cs
+++ SalesManagement_SysDev/Form/F_SearchDialog.Designer.cs
@@ -30,42 +30,71 @@
         {
             this.btnAndSearch = new System.Windows.Forms.Button();
             this.btnOrSearch = new System.Windows.Forms.Button();
+            this.lblAndSearch = new System.Windows.Forms.Label();
+            this.lblOrSearch = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // btnAndSearch
             // 
-            this.btnAndSearch.Font = new System.Drawing.Font("MS UI Gothic", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
-            this.btnAndSearch.Location = new System.Drawing.Point(46, 92);
+            this.btnAndSearch.Font = new System.Drawing.Font("MS UI Gothic", 30F);
+            this.btnAndSearch.Location = new System.Drawing.Point(288, 276);
+            this.btnAndSearch.Margin = new System.Windows.Forms.Padding(2);
             this.btnAndSearch.Name = "btnAndSearch";
-            this.btnAndSearch.Size = new System.Drawing.Size(154, 68);
+            this.btnAndSearch.Size = new System.Drawing.Size(265, 117);
             this.btnAndSearch.TabIndex = 0;
             this.btnAndSearch.Text = "AND検索";
             this.btnAndSearch.UseVisualStyleBackColor = true;
             // 
             // btnOrSearch
             // 
-            this.btnOrSearch.Font = new System.Drawing.Font("MS UI Gothic", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
-            this.btnOrSearch.Location = new System.Drawing.Point(281, 92);
+            this.btnOrSearch.Font = new System.Drawing.Font("MS UI Gothic", 30F);
+            this.btnOrSearch.Location = new System.Drawing.Point(288, 651);
+            this.btnOrSearch.Margin = new System.Windows.Forms.Padding(2);
             this.btnOrSearch.Name = "btnOrSearch";
-            this.btnOrSearch.Size = new System.Drawing.Size(154, 68);
+            this.btnOrSearch.Size = new System.Drawing.Size(265, 117);
             this.btnOrSearch.TabIndex = 1;
             this.btnOrSearch.Text = "OR検索";
             this.btnOrSearch.UseVisualStyleBackColor = true;
             // 
+            // lblAndSearch
+            // 
+            this.lblAndSearch.AutoSize = true;
+            this.lblAndSearch.Font = new System.Drawing.Font("MS UI Gothic", 30F);
+            this.lblAndSearch.Location = new System.Drawing.Point(638, 314);
+            this.lblAndSearch.Name = "lblAndSearch";
+            this.lblAndSearch.Size = new System.Drawing.Size(502, 40);
+            this.lblAndSearch.TabIndex = 2;
+            this.lblAndSearch.Text = "「全てのキーワードを含む」検索";
+            // 
+            // lblOrSearch
+            // 
+            this.lblOrSearch.AutoSize = true;
+            this.lblOrSearch.Font = new System.Drawing.Font("MS UI Gothic", 30F);
+            this.lblOrSearch.Location = new System.Drawing.Point(638, 689);
+            this.lblOrSearch.Name = "lblOrSearch";
+            this.lblOrSearch.Size = new System.Drawing.Size(566, 40);
+            this.lblOrSearch.TabIndex = 3;
+            this.lblOrSearch.Text = "「いずれかのキーワードを含む」検索";
+            // 
             // F_SearchDialog
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(487, 215);
+            this.ClientSize = new System.Drawing.Size(1904, 1041);
             this.ControlBox = false;
+            this.Controls.Add(this.lblOrSearch);
+            this.Controls.Add(this.lblAndSearch);
             this.Controls.Add(this.btnOrSearch);
             this.Controls.Add(this.btnAndSearch);
-            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Margin = new System.Windows.Forms.Padding(2);
             this.MaximizeBox = false;
             this.MinimizeBox = false;
             this.Name = "F_SearchDialog";
             this.Text = "F_SearchDialog";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -73,5 +102,7 @@
 
         private System.Windows.Forms.Button btnAndSearch;
         private System.Windows.Forms.Button btnOrSearch;
+        private System.Windows.Forms.Label lblAndSearch;
+        private System.Windows.Forms.Label lblOrSearch;
     }
 }
